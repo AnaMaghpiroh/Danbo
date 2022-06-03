e915e45364b601c702c7b0fe059a073db5816574
AnaMaghpiroh <106145282+AnaMaghpiroh@users.noreply.github.com>
2022-06-03 23:44:16 +0700

    Update Danbo.cpp
    Menambahkan dari baris 7 sampai baris 19

--- Danbo.cpp
+++ Danbo.cpp
@@ -6,3 +6,14 @@
 
 using namespace std;
 
+int main (){
+  //deklarasi variabel
+  float pi =3.14
+    
+    //deklarasi molekul kadmium klorida
+    float Cdx, Cdy, Cdz;
+    float Cl1x, Cl1y, Cl1z;
+    float Cl2x, Cl2y, Cl2z;
+    
+    //masukkan nilai untuk setiap variabel CdCl2
+  Cdx = Cdy = Cdz = 
