2ea8a73de5c434037851c120c948603a94acdf0b
AnaMaghpiroh <106145282+AnaMaghpiroh@users.noreply.github.com>
2022-06-07 07:41:12 +0700

    Update Kadmium klorida.cpp
    Menambahkan dari baris  18 sampai dengan baris 40

--- Kadmium klorida.cpp
+++ Kadmium klorida.cpp
@@ -9,11 +9,37 @@ using namespace std;
 int main (){
   //deklarasi variabel
   float pi =3.14
+  int counter =0;
     
     //deklarasi molekul kadmium klorida
     float Cdx, Cdy, Cdz;
     float Cl1x, Cl1y, Cl1z;
     float Cl2x, Cl2y, Cl2z;
+  
+    /*  Cl     Cl
+          \   /
+            Cd 
+        molekul kadmium klorida itu bentuk liner
+    */
+  
+  float rB_Cdx = 0,0;
+  float rB_Cdy = 0.0;
+  float rB_Cdz = 0.0;
+  
+  float rB_Clx =sin (2.42*pi/180) * 2.420;
+  float rB_Cly =0.0;
+  float rB_Clz =cos(2.42*pi/180)  * 2.420;
+  
+  float rB_Cl2x =- rB_Cl1x;
+  float rB_Cl2z = rB_Cl1y;
+  float rB_Cl2z = rB_Cl1z;
+  
+  //iterasi untuk setiap molekul kadmium klorida
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (interaksi k =0; k < 3; k++) {
+        
+    
     
     //masukkan nilai untuk setiap variabel CdCl2
   Cdx = Cdy = Cdz = 0.0;
@@ -30,7 +56,7 @@ int main (){
     ofstream file;
     file.open("kadmium klorida");
     file << "3\n"<< endl;
-    file << setw(3)<<" Cd"<<setw(3)<<""\
+    file << setw(3)<<" Cd"<<setw(3)<<"" \
     << fixed << setprecision(3) << Cdx << setw(3)<<""\
     << fixed << setpracision(3) << Cdy << setw(3)<<""\
     << fixed << setpracision(3) << Cdz << "\n";
