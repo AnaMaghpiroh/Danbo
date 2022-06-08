220c86b1d43a33cab47944e3ebc16502127f3792
Putrirahayuputri <106719478+Putrirahayuputri@users.noreply.github.com>
2022-06-08 20:11:57 +0700

    Update Kadmium klorida.cpp
    Menambahkan writefile ep Cdcl2 sampai float Mr cdcl2

--- Kadmium klorida.cpp
+++ Kadmium klorida.cpp
@@ -106,3 +106,22 @@ int main (){
 
     return 0;
 }
+ 
+    %%writerfile ep-CdCl2.cpp
+    
+    #imclude "iostream"
+    #include "cmath"
+    #include "iomanip"
+    #include "fstream"
+    
+    using namespace std;
+
+    float densitas;         /*ensitas ini memiliki satuan g cm^-3
+                               dan ini harus diinput dari user */
+    // converter dari nilai cm^3 ke A^3
+    float cm^3_to_A3=1.0E-24;
+    float numb_mol;
+    float Na = 6.022E23;            // bilangan avogardo
+    float Mr_CdCl2 = 111;           // massa relatif dari CdCl2
+    
+
