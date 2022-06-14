006537d1dc2558e86c25fac83b7d9724f67b2ab1
AnaMaghpiroh <106145282+AnaMaghpiroh@users.noreply.github.com>
2022-06-14 12:43:14 +0700

    Update Kadmium klorida.cpp
    Menambahkan baris 101 sampai baris 103

--- Kadmium klorida.cpp
+++ Kadmium klorida.cpp
@@ -1,4 +1,6 @@
-%%writefile ep-kadmiumdiklorida-ruah.cpp
+
+    
+%%writefile ep-Kadmiumdiklorida-ruah.cpp
 
 #include "iostream"
 #include "cmath"
@@ -7,16 +9,15 @@
 
 using namespace std;
 
-
-int main (){
-  //deklarasi variabel
-  float pi =3.14
-  int counter =0;
+int main(){
+    // deklarasi variabel
+    float pi = 3.14;
+    int counter = 0;
     
-    //deklarasi molekul kadmium klorida
-    float Cdx[N], Cdy[N], Cdz[N];
-    float Cl1x[N], Cl1y[N], Cl1z[N];
-    float Cl2x[N], Cl2y[N], Cl2z[N];
+    // deklarasi molekul kadmiumdiklorida
+    float Cdx[125], Cdy[125], Cdz[125];
+    float Cl1x[125], Cl1y[125], Cl1z[125];
+    float Cl2x[125], Cl2y[125], Cl2z[125];
   
     /*  Cl- Cd - Cl
         molekul kadmiumdiklorida itu bentuk linier
@@ -26,185 +27,80 @@ int main (){
   float rB_Cdy = 0.0;
   float rB_Cdz = 0.0;
   
-  float rB_Clx =sin (2.42*pi/180) * 4.840;
-  float rB_Cly =0.0;
-  float rB_Clz =cos(2.42*pi/180)  * 4.840;
+  float rB_Cl1x = sin (2.42*pi/180) * 4.840;
+  float rB_Cl1y = 0.0;
+  float rB_Cl1z = cos(2.42*pi/180)  * 4.840;
   
-  float rB_Cl2x =- rB_Cl1x;
-  float rB_Cl2z = rB_Cl1y;
+  float rB_Cl2x = - rB_Cl1x;
+  float rB_Cl2y = rB_Cl1y;
   float rB_Cl2z = rB_Cl1z;
 
-  //iterasi untuk setiap molekul kadmiumdiklorida
+  //iterasi untuk setiap molekul kadmium diklorida
   for (int i = 0; i < 3; i++){
     for (int j = 0; j < 3; j++){
-      for (interaksi k =0; k < 3; k++){
+      for (int k =0; k < 3; k++){
  
-  int counter =0;
-    for(int i = 0; i <numb_lat; i++){//x
-      for(int j = 0; j < numb_lat; j++){//y
-        for(int k = 0; k < numb_lat; k++){//z
-
-  Cx[counter] = rB_Cx + (i*lat);
-  Cy[counter] = rB_Cy + (k*lat);
-  Cz[counter] = rB_Cz + (k*lat);
+          rB_Cdx ;counter;rB_Cdx + (i*3.01);
+          rB_Cdy ;counter;rB_Cdy + (k*3.01);
+          rB_Cdz ;counter;rB_Cdz + (k*3.01);
     
-    //masukkan nilai untuk setiap variabel CdCl2
-  Cdx = Cdy = Cdz = 0.0;
+          rB_Cl1x = sin(2.42*pi/180)*2.420;
+          rB_Cl1y = 0.0;
+          rB_Cl1z = cos(2.42*pi/180)*2.420;
     
-    Cl1x =sin(2.42*pi/180)*4.840;
-    Cl1y =0.0;
-    Cl1z =cos(2.42*pi/180)*4.840;
+          rB_Cl2x = - rB_Cl1x;
+          rB_Cl2y = rB_Cl1y;
+          rB_Cl2z = rB_Cl1z;
     
-    Cl2x =-Clx;
-    Cl2y =Cly;
-    Cl2z =Clz;
-    
-    Cdx[counter] = rB_Cdx + (i*3.01);
-    Cdy[counter] = rB_Cdy + (j*3.01);
-    Cdz[counter] = rB_Cdz + (k*3.01);
+          rB_Cdx ;counter;rB_Cdx + (i*3.01);
+          rB_Cdy ;counter;rB_Cdy + (j*3.01);
+          rB_Cdz ;counter;rB_Cdz + (k*3.01);
  
-    Cl1x[counter] = Cdx[counter] + rB_Cl1x;
-    Cl1y[counter] = Cdy[counter] + rB_Cl1y;
-    Cl1z[counter] = Cdz[counter] + rB_Cl1z;
+           rB_Cl1x ;counter:rB_Cdx ;counter; + rB_Cl1x;
+           rB_Cl1y ;counter;rB_Cdy ;counter; + rB_Cl1y;
+           rB_Cl1z ;counter;rB_Cdz ;counter; + rB_Cl1z;
 
-    Cl2x[counter] = Cdx[counter] + rB_Cl2x;
-    Cl2y[counter] = Cdy[counter] + rB_Cl2y;
-    Cl2z[counter] = Cdz[counter] + rB_Cl2z;
+           rB_Cl2x ;counter;rB_Cdx ;counter; + rB_Cl2x;
+           rB_Cl2y ;counter;rB_Cdy ;counter; + rB_Cl2y;
+           rB_Cl2z ;counter;rB_Cdz ;counter; + rB_Cl2z;
 
-    counter += 1;
-     }
-   }
- }   
-
+           counter += 1;
+         }
+      }
+   }   
     //hasil iterasi dimasukkan ke dalam file xyz
     ofstream file;
     file.open("kadmium-diklorida.xyz");
-    int N = 125 * 5;
+    int N = 125 * 3;
     file << N << "\n"<< endl;
 
     // iterasi dalam output array
-    for(int m = 0; m < 125; m++)
-
-     
-    //memasukkan nilai tersebut ke dalam file
-    ofstream file;
-    file.open("kadmium diklorida");
-    file << "3\n"<< endl;
-    file << setw(3)<<" Cd"<<setw(3)<<"" \
-    << fixed << setprecision(3) << Cdx << setw(3)<<""\
-    << fixed << setprecision(3) << Cdy << setw(3)<<""\
-    << fixed << setprecision(3) << Cdz << "\n";
+    for(int m = 0; m < 125; m++){
+      file << setw(3) << "Cd" << setw(3) <<" " \
+           << fixed << setprecision(3) << rB_Cdx << setw(3) <<" "\
+           << fixed << setprecision(3) << rB_Cdy << setw(3) <<" "\
+           << fixed << setprecision(3) << rB_Cdz << "\n";
     
-    file << setw(3) << "Cl1" << setw(3) << " " \
-    << fixed << setprecision(3) << Cl1x << setw(3) << " "\
-    << fixed << setprecision(3) << Cl1y << setw(3) << " "\
-    << fixed << setprecision(3) << Cl1z << "\n";
+      file << setw(3) << "Cl" << setw(3) <<" " \
+           << fixed << setprecision(3) << rB_Cl1x << setw(3) <<" "\
+           << fixed << setprecision(3) << rB_Cl1y << setw(3) <<" "\
+           << fixed << setprecision(3) << rB_Cl1z << "\n";
 
-    file << setw(3) << "Cl2” << setw(3) << " " \
-    << fixed << setprecision(3) << Cl2x << setw(3) << " "\
-    << fixed << setprecision(3) << Cl2y << setw(3) << " "\
-    << fixed << setprecision(3) << Cl2z << "\n";
-
+      file << setw(3) << "Cl" << setw(3) <<" " \
+           << fixed << setprecision(3) << rB_Cl2x  << setw(3) <<" "\
+           << fixed << setprecision(3) << rB_Cl2y << setw(3) <<" "\
+           << fixed << setprecision(3) << rB_Cl2z << "\n";
+    }
     file.close();
     
-    cout << "program selesai";
+    cout << "program telah selesai";
 
     return 0;
 }
- 
-    %%shell
-    
-    g++ ep-kadmiumdiklorida-ruah. cpp-o kadmiumdiklorida
-    . /ep
-    
 
-    %%writerfile ep-CdCl2.cpp
-    
-    #imclude "iostream"
-    #include "cmath"
-    #include "iomanip"
-    #include "fstream"
-    
-    using namespace std;
+%%shell 
 
-    float densitas;         /*ensitas ini memiliki satuan g cm^-3
-                               dan ini harus diinput dari user */
-    // converter dari nilai cm^3 ke A^3
-    float cm^3_to_A3=1.0E-24;
-    float numb_mol;
-    float Na = 6.022E23;            // bilangan avogardo
-    float Mr_CdCl2 = 111;           // massa relatif dari kadmium klorida
-    int numb_lat;                   // panjang sel simulasi
-    int N;                          // jumlah molekul
-    float volume;                   // volume dari molekul kadmium klorida
-    float Ix, Iy, Iz;               // panjang sel simulasi terhitung
-    float lat;                      // jarak antar molekul kadmium klorida
-
-    // variabel perhitungan energi potensial
-    float sigma = 2.725;             // satuan (A) 
-    float epsilon = 3.725;           // satuan (kj/mol)
-    float rij, rcut, ruct2;
-    float Ep,Ep_Lj, A12, B6 ;
-    float dx, dy,dz;
-   
-    int main(){
-      
-      rcut = 3* sigma
-        rcut2 = pow (rcut, 2);
-     
-      cout<<"program menghitung energi potensial dari CdCl2"
-      >>endl;
-      cout<<"masukkan nilai densitas CdCl2(eg.10);
-      cin>>densitas;
-      
-      numb_mol=densitas*(Na/Mr_CdCl2)*cm3_to_A3;
-      cout<<"masukan panjang sel simulasi(eg.5):";
-      cin>>numb_lat 
-      N=(paw(numb_lat,3))*3;
-      volum=(float(N/numb_mol;
-                   
-      //panjang sel simulasi secara perhitungan 
-      lx=pow(volum,(1.0/3.0));
-      ly=lx;
-      lz=lx;
-      if(lx<(2*rcut)){
-        cout<<"sory mazeh,sel simulasi kamu besar";
-        exit(0);
-      } else{
-        cout<<"panjang sel simulasi baru:"<<lx<<endl;}
-        lat=lx/(float)numb_lat;
-                   
-        //deklarasi variabel float pi=3.14;
-
-     //deklarasi molekul CdCl2
-     float Cdx[125], Cdy[125], Cdz[125];
-     float Cl1x[125], Cl1y[125], Cl1z[125];
-     float Cl2x[125], Cl2y[125], Cl2z[125];
-
-     N];
-  
-    /*  Cl — cd — Cl
-
-
-     molekul kadmium klorida itu bentuk liner
-    */
-  
-  float rB_Cdx = 0,0;
-  float rB_Cdy = 0.0;
-  float rB_Cdz = 0.0;
-  
-  float rB_Clx =sin (2.42*pi/180) * 2.420;
-  float rB_Cly =0.0;
-  float rB_Clz =cos(2.42*pi/180)  * 2.420;
-  
-  float rB_Cl2x =- rB_Cl1x;
-  float rB_Cl2z = rB_Cl1y;
-  float rB_Cl2z = rB_Cl1z;
-
-  
-  
-
-
+ g++ ep-Kadmiumdiklorida-ruah.cpp -o Kadmiumdiklorida
 
 
 
